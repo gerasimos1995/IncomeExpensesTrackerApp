701b4b275c6f8849342daa74f6e65f686f016d35
GerasimosRagkavanis <gerasimos_ragavanis@yahoo.gr>
2020-12-28 21:35:50 +0200

    Added firebase and registration functionallity

--- app/src/main/java/com/example/expensestrackerapp/MainActivity.java
+++ app/src/main/java/com/example/expensestrackerapp/MainActivity.java
@@ -58,5 +58,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
             }
         });
+
+        // If user has forgotten password got to reset activity
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResetActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/expensestrackerapp/RegistrationActivity.java
+++ app/src/main/java/com/example/expensestrackerapp/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.example.expensestrackerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,50 +11,80 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText email;
-    private EditText password;
-    private Button btnSignup;
-    private TextView already_user;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mBtnSignup;
+    private TextView mAlready_user;
+
+    private ProgressDialog mDialog;
+
+    // Firebase
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mDialog = new ProgressDialog(this);
+        fAuth = FirebaseAuth.getInstance();
         register();
     }
 
     private void register(){
-        email = findViewById(R.id.email_register);
-        password = findViewById(R.id.password_register);
-        btnSignup = findViewById(R.id.btn_sign_up);
-        already_user = findViewById(R.id.already_user);
+        mEmail = findViewById(R.id.email_register);
+        mPassword = findViewById(R.id.password_register);
+        mBtnSignup = findViewById(R.id.btn_sign_up);
+        mAlready_user = findViewById(R.id.already_user);
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        mBtnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String memail = email.getText().toString().trim();
-                String mpassword = password.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
-                if (TextUtils.isEmpty(memail)){
-                    email.setError("Email Required.."); // I could add a toast msg
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email Required.."); // I could add a toast msg
                     return;
                 }
 
-                if (TextUtils.isEmpty(mpassword)){
-                    password.setError("Password Required");
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password Required");
                     return;
                 }
                 // I can add more validations here
+
+                mDialog.setMessage("Processing");
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }else{
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Registration Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
 
 
         // Change activity to login because user has an account already
-        already_user.setOnClickListener(new View.OnClickListener() {
+        mAlready_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
